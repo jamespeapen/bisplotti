5b6e511f2cd22e941412d6420a6f3ede036be8f4
James Eapen <james.eapen@vai.org>
2022-06-15 17:53:05 -0400

    switch to lapply - incorrect output

--- wzmetagene.r
+++ wzmetagene.r
@@ -34,7 +34,6 @@ wzmetagene <- function(bedfile,
                        fold = F,
                        strand = NULL,
                        ignoreend = F) {
-
   opts <- list(
     middle = middle,
     outer = outer,
@@ -62,13 +61,13 @@ wzmetagene <- function(bedfile,
   r <- NULL
   r2 <- NULL
 
-  header <- c("chr", "start", "end", "strand")
   input_bed <- read.table(bedfile, header = F)
+  header <- c("chr", "start", "end", "strand")
   if (ncol(input_bed) == 3) {
     colnames(input_bed) <- header[1:3]
     opts$strand <- NULL
   } else {
-      colnames(input_bed) <- header
+    colnames(input_bed) <- header
   }
 
   # preserve original start and end
@@ -84,13 +83,14 @@ wzmetagene <- function(bedfile,
   input_bed$step1 <- flankstep
   input_bed$step2 <- flankstep
 
-  for (i in 1:nrow(input_bed)) {
+  ctcf <- lapply(seq_len(nrow(input_bed)), function(i) {
     cur_record <- input_bed[i, ]
     prev_record <- input_bed[i - 1, ]
     next_record <- input_bed[i + 1, ]
-
-    process_record(cur_record, process_record, next_record, opts)
-  }
+    process_record(cur_record, prev_record, next_record, opts)
+  })
+  write.table(do.call(rbind, ctcf), "result.txt", row.names=F, col.names=F, quote=F, sep="\t")
+  
 }
 
 #' Parse target region, creating windows based on the flanking inputs given on the command line
@@ -99,10 +99,10 @@ wzmetagene <- function(bedfile,
 #' @param previous_record Previous record in file
 #' @param next_record     Next record in file
 #' @param opts            List of options for wzmetagene
+#' @param ctcf_table      Result data.table object
 #'
 #' @return NULL
 process_record <- function(cur_record, previous_record, next_record, opts) {
-
   if (opts$flankbygene) {
     cur_record$step1 <- (cur_record$end - cur_record$start + 1) / opts$numinternal
     cur_record$step2 <- cur_record$step1
@@ -143,19 +143,21 @@ process_record <- function(cur_record, previous_record, next_record, opts) {
   }
 
   if (opts$fold) {
-    sample_backward(cur_record, function(i) { -i - 1 }, opts)
-    sample_internal(cur_record, function(i) { min(i, opts$numinternal - 1 - i) }, opts)
-    sample_forward(cur_record, function(i) { -i - 1 } , opts)
+    sb_fold <- sample_backward(cur_record, function(i) { -i - 1 }, opts)
+    si_fold <- sample_internal(cur_record, function(i) { min(i, opts$numinternal - 1 - i) }, opts)
+    sf_fold <- sample_forward(cur_record, function(i) { -i - 1 }, opts)
   }
 
   if (strand == "+") {
-    sample_backward(cur_record, function(i) { -i }, opts) # removed -1 since 1 base
-    sample_internal(cur_record, function(i) { i - 1 }, opts) # -1 to offset 1 base and get a 0 start
-    sample_forward(cur_record, function(i) { opts$numinternal + i }, opts)
+    sb <- sample_backward(cur_record, function(i) { -i }, opts) # removed -1 since 1 base
+    si <- sample_internal(cur_record, function(i) { i - 1 }, opts) # -1 to offset 1 base and get a 0 start
+    sf <- sample_forward(cur_record, function(i) { opts$numinternal + i }, opts)
+    return(rbind(sb, si, sf))
   } else {
-    sample_forward(cur_record, function(i) { -i - 1 }, opts)
-    sample_internal(cur_record, function(i) { opts$numinternal - i}, opts)
-    sample_backward(cur_record, function(i) { opts$numinternal + i - 1 }, opts)
+    sf <- sample_forward(cur_record, function(i) { -i - 1 }, opts)
+    si <- sample_internal(cur_record, function(i) { opts$numinternal - i }, opts)
+    sb <- sample_backward(cur_record, function(i) { opts$numinternal + i - 1 }, opts)
+    return(rbind(sf, si, sb))
   }
 }
 
@@ -165,133 +167,20 @@ process_record <- function(cur_record, previous_record, next_record, opts) {
 #' @param index_func function for adjusting the index value for the bin
 #' @param opts       List of options for wzmetagene
 #'
-#' @return NULL
+#' @return vector of chr, window_beg, window_end, index, reg_start, reg_end, area, chr, og_start, og_end, strand
 sample_backward <- function(cur_record, index_func, opts) {
-
   if (cur_record$step1 == 0 || opts$flanknumber == 0) {
     return()
   }
 
   temp_window_end <- cur_record$start
 
-  for (i in 1:opts$flanknumber) {
+  result <- lapply(seq_len(opts$flanknumber), function(i) {
     temp_window_beg <- temp_window_end - cur_record$step1
 
     if (opts$outer) {
       window_beg <- as.integer(temp_window_beg)
       window_end <- window_beg + 1
-    } else if (opts$middle) {
-        window_mid <- as.integer((temp_window_beg + temp_window_end) / 2)
-        window_beg <- window_mid - 1
-        window_end <- window_mid
-    } else {
-        window_beg <- as.integer(temp_window_beg)
-        window_end <- as.integer(temp_window_end)
-    }
-
-    index <- index_func(i)
-    if (index < 0) {
-      if (opts$flankbygene) {
-        reg <- sprintf("(%d)-(%d)", -i - 1, -i)
-      } else if (opts$flanktoneighbor) {
-        if (opts$fold) {
-          reg <- sprintf(
-            "(%d)-(%d)",
-            as.integer(-i - 1 / opts$flanknumber / 2 * 100),
-            as.integer(-i / opts$flanknumber / 2 * 100)
-          )
-        } else {
-          reg <- sprintf(
-            "(%d)-(%d)",
-            as.integer(-i - 1 / opts$flanknumber * 100),
-            as.integer(-i / opts$flanknumber * 100)
-          )
-        }
-      } else {
-        reg <- sprintf(
-            "(%d)-(%d)",
-            as.integer(window_beg - cur_record$start),
-            as.integer(window_end - cur_record$start)
-        )
-      }
-    } else {
-      if (opts$flankbygene) {
-        reg <- sprintf("%d-%d", i, i + 1)
-      } else if (opts$flanktoneighbor) {
-        if (opts$fold) {
-          reg <- sprintf(
-            "%d-%d",
-            as.integer(i / opts$flanknumber / 2 * 100),
-            as.integer(i + 1 / opts$flanknumber / 2 * 100)
-          )
-        } else {
-          reg <- sprintf(
-            "%d-%d",
-            as.integer(i / opts$flanknumber * 100),
-            as.integer(i + 1 / opts$flanknumber * 100)
-          )
-        }
-      } else {
-        reg <- sprintf(
-          "%d-%d",
-          as.integer(cur_record$start - window_end),
-          as.integer(cur_record$start - window_beg)
-        )
-      }
-    }
-
-    if (index >= 0) {
-      area <- 1
-    } else {
-      area <- -1
-    }
-
-    if (opts$expansion > 0) {
-      window_beg <- max(window_beg - opts$expansion, 0)
-      window_end <- window_end + opts$expansion
-    }
-
-    if (window_beg > 0 && window_end > window_beg) {
-      (sprintf(
-          "%s\t%d\t%d\t%s\t%s\t%s\t%s\t%s\t%s\t%s\n",
-          cur_record$chr,
-          as.integer(window_beg),
-          as.integer(window_end),
-          index,
-          reg,
-          area,
-          cur_record$chr,
-          cur_record$og_start,
-          cur_record$og_end,
-          cur_record$strand
-      ))
-    }
-
-    temp_window_end <- temp_window_beg
-  }
-}
-
-#' Move towards the end of the chromosome (relative to the reference) from target region
-#'
-#' @param cur_record the record being sampled
-#' @param index_func function for adjusting the index value for the bin
-#' @param opts       List of options for wzmetagene
-#'
-#' @return NULL
-sample_forward <- function(cur_record, index_func, opts) {
-
-  if (cur_record$step2 < 0 || opts$flanknumber == 0) {
-    return ()
-  }
-
-  temp_window_beg <- cur_record$end
-
-  for (i in 1:opts$flanknumber - 1) {
-    temp_window_end <- temp_window_beg + cur_record$step2
-
-    if (opts$outer) {
-      window_end <- as.integer(temp_window_end)
-      window_beg <- window_end - 1
     } else if (opts$middle) {
       window_mid <- as.integer((temp_window_beg + temp_window_end) / 2)
       window_beg <- window_mid - 1
@@ -302,45 +191,37 @@ sample_forward <- function(cur_record, index_func, opts) {
     }
 
     index <- index_func(i)
-
     if (index < 0) {
       if (opts$flankbygene) {
-        reg <- sprintf("(%d)-(%d)", -i - 1, -i)
+        reg_start <- -i - 1
+        reg_end <- -i
       } else if (opts$flanktoneighbor) {
         if (opts$fold) {
-          reg <- sprintf(
-            "(%d)-(%d)",
-            as.integer(-i - 1 / opts$flanknumber / 2 * 100),
-            as.integer(-i / opts$flanknumber / 2 * 100)
-          )
+          reg_start <- as.integer(-i - 1 / opts$flanknumber / 2 * 100)
+          reg_end <- as.integer(-i / opts$flanknumber / 2 * 100)
         } else {
-          reg <- sprintf(
-            "(%d)-(%d)",
-            as.integer(-i - 1 / opts$flanknumber * 100),
-            as.integer(-i / opts$flanknumber * 100)
-          )
+          reg_start <- as.integer(-i - 1 / opts$flanknumber * 100)
+          reg_end <- as.integer(-i / opts$flanknumber * 100)
         }
       } else {
-        reg <- sprintf(
-          "(%d)-(%d)",
-          as.integer(cur_record$end - window_end),
-          as.integer(cur_record$end - window_beg)
-        )
+        reg_start <- as.integer(window_beg - cur_record$start)
+        reg_end <- as.integer(window_end - cur_record$start)
       }
     } else {
       if (opts$flankbygene) {
-        reg <- sprintf("%d-%d", i, i + 1)
+        reg_start <- i
+        reg_end <- i + 1
       } else if (opts$flanktoneighbor) {
-        reg <- sprintf(
-          "%d-%d",
-          as.integer(i / opts$flanknumber * 100),
-          as.integer((i + 1) / opts$flanknumber * 100)
-        )
+        if (opts$fold) {
+          reg_start <- as.integer(i / opts$flanknumber / 2 * 100)
+          reg_end <- as.integer(i + 1 / opts$flanknumber / 2 * 100)
+        } else {
+          reg_start <- as.integer(i / opts$flanknumber * 100)
+          reg_end <- as.integer(i + 1 / opts$flanknumber * 100)
+        }
       } else {
-        reg <- sprintf("%d-%d",
-            as.integer(window_beg - cur_record$end),
-            as.integer(window_end - cur_record$end)
-        )
+        reg_start <- as.integer(cur_record$start - window_end)
+        reg_end <- as.integer(cur_record$start - window_beg)
       }
     }
 
@@ -355,24 +236,116 @@ sample_forward <- function(cur_record, index_func, opts) {
       window_end <- window_end + opts$expansion
     }
 
+    temp_window_end <- temp_window_beg
+
     if (window_beg > 0 && window_end > window_beg) {
-      (sprintf(
-        "%s\t%d\t%d\t%s\t%s\t%s\t%s\t%s\t%s\t%s\n",
+      c(
         cur_record$chr,
         as.integer(window_beg),
         as.integer(window_end),
         index,
-        reg,
+        reg_start,
+        reg_end,
         area,
         cur_record$chr,
         cur_record$og_start,
         cur_record$og_end,
         cur_record$strand
-      ))
+      )
     }
+  })
+  return(do.call(rbind, result))
+}
 
-    temp_window_beg <- temp_window_end
+#' Move towards the end of the chromosome (relative to the reference) from target region
+#'
+#' @param cur_record the record being sampled
+#' @param index_func function for adjusting the index value for the bin
+#' @param opts       List of options for wzmetagene
+#'
+#' @return NULL
+sample_forward <- function(cur_record, index_func, opts) {
+  if (cur_record$step2 < 0 || opts$flanknumber == 0) {
+    return()
   }
+
+  temp_window_beg <- cur_record$end
+    result <- lapply(seq_len(opts$flanknumber - 1), function(i) {
+      temp_window_end <- temp_window_beg + cur_record$step2
+
+      if (opts$outer) {
+        window_end <- as.integer(temp_window_end)
+        window_beg <- window_end - 1
+      } else if (opts$middle) {
+        window_mid <- as.integer((temp_window_beg + temp_window_end) / 2)
+        window_beg <- window_mid - 1
+        window_end <- window_mid
+      } else {
+        window_beg <- as.integer(temp_window_beg)
+        window_end <- as.integer(temp_window_end)
+      }
+
+      index <- index_func(i)
+
+      if (index < 0) {
+        if (opts$flankbygene) {
+          reg_start <- -i - 1
+          reg_end <- -i
+        } else if (opts$flanktoneighbor) {
+          if (opts$fold) {
+            reg_start <- as.integer(-i - 1 / opts$flanknumber / 2 * 100)
+            reg_end <- as.integer(-i / opts$flanknumber / 2 * 100)
+          } else {
+            reg_start <- as.integer(-i - 1 / opts$flanknumber * 100)
+            reg_end <- as.integer(-i / opts$flanknumber * 100)
+          }
+        } else {
+          reg_start <- as.integer(cur_record$end - window_end)
+          reg_end <- as.integer(cur_record$end - window_beg)
+        }
+      } else {
+        if (opts$flankbygene) {
+          reg_start <- i
+          reg_end <- i + 1
+        } else if (opts$flanktoneighbor) {
+          reg_start <- as.integer(i / opts$flanknumber * 100)
+          reg_end <- as.integer((i + 1) / opts$flanknumber * 100)
+        } else {
+          reg_start <- as.integer(window_beg - cur_record$end)
+          reg_end <- as.integer(window_end - cur_record$end)
+        }
+      }
+
+      if (index >= 0) {
+        area <- 1
+      } else {
+        area <- -1
+      }
+
+      if (opts$expansion > 0) {
+        window_beg <- max(window_beg - opts$expansion, 0)
+        window_end <- window_end + opts$expansion
+      }
+
+      temp_window_beg <- temp_window_end
+
+      if (window_beg > 0 && window_end > window_beg) {
+        c(
+          cur_record$chr,
+          as.integer(window_beg),
+          as.integer(window_end),
+          index,
+          reg_start,
+          reg_end,
+          area,
+          cur_record$chr,
+          cur_record$og_start,
+          cur_record$og_end,
+          cur_record$strand
+        )
+      }
+      })
+  return(do.call(rbind, result))
 }
 
 #' Move within the target region
@@ -383,53 +356,15 @@ sample_forward <- function(cur_record, index_func, opts) {
 #'
 #' @return NULL
 sample_internal <- function(cur_record, index_func, opts) {
-
   if (opts$outer) {
-
     sentinels <- seq(cur_record$start + 1, cur_record$end, length.out = opts$numinternal)
 
-    for (i in 1:length(sentinels)) {
-      window_end <- as.integer(sentinels[i])
-      window_beg <- window_end - 1
+      result <- lapply(seq_len(length(sentinels)), function(i) {
 
-      if (opts$expansion) {
-        window_beg <- max(window_beg - opts$expansion, 0)
-        window_end <- window_end + opts$expansion
-      }
+        window_end <- as.integer(sentinels[i])
+        window_beg <- window_end - 1
 
-      index <- index_func(i)
-
-      if (window_beg > 0 && window_end > window_beg) {
-        (sprintf("%s\t%d\t%d\t%s\t%d-%d\t0\t%s\t%s\t%s\t%s\n",
-          cur_record$chr,
-          as.integer(window_beg),
-          as.integer(window_end),
-          index,
-          as.integer(index / opts$numinternal * 100),
-          as.integer((index + 1) / opts$numinternal * 100),
-          cur_record$chr,
-          cur_record$og_start,
-          cur_record$og_end,
-          cur_record$strand
-        ))
-      }
-    }
-  } else {
-
-    sentinels <- seq(cur_record$start, cur_record$end, length.out = opts$numinternal + 1)
-    if (length(sentinels) < 2) return()
-
-      for (i in 1:(length(sentinels) - 1)) {
-        window_beg <- as.integer(sentinels[i])
-        window_end <- as.integer(sentinels[i + 1])
-
-        if (opts$middle) {
-          window_mid <- as.integer((sentinels[i] + sentinels[i + 1]) / 2)
-          window_beg <- window_mid - 1
-          window_end <- window_mid
-        }
-
-        if (opts$expansion > 0) {
+        if (opts$expansion) {
           window_beg <- max(window_beg - opts$expansion, 0)
           window_end <- window_end + opts$expansion
         }
@@ -437,19 +372,61 @@ sample_internal <- function(cur_record, index_func, opts) {
         index <- index_func(i)
 
         if (window_beg > 0 && window_end > window_beg) {
-          (sprintf("%s\t%d\t%d\t%s\t%d-%d\t0\t%s\t%s\t%s\t%s\n",
+          c(
             cur_record$chr,
             as.integer(window_beg),
             as.integer(window_end),
             index,
             as.integer(index / opts$numinternal * 100),
             as.integer((index + 1) / opts$numinternal * 100),
+            0L,
             cur_record$chr,
             cur_record$og_start,
             cur_record$og_end,
             cur_record$strand
-          ))
+          )
         }
+    })
+  return(do.call(rbind, result))
+  } else {
+    sentinels <- seq(cur_record$start, cur_record$end, length.out = opts$numinternal + 1)
+    if (length(sentinels) < 2) {
+      return()
+    }
+
+    result <- lapply(seq_len(length(sentinels) - 1), function(i) {
+      window_beg <- as.integer(sentinels[i])
+      window_end <- as.integer(sentinels[i + 1])
+
+      if (opts$middle) {
+        window_mid <- as.integer((sentinels[i] + sentinels[i + 1]) / 2)
+        window_beg <- window_mid - 1
+        window_end <- window_mid
       }
+
+      if (opts$expansion > 0) {
+        window_beg <- max(window_beg - opts$expansion, 0)
+        window_end <- window_end + opts$expansion
+      }
+
+      index <- index_func(i)
+
+      if (window_beg > 0 && window_end > window_beg) {
+        c(
+          cur_record$chr,
+          as.integer(window_beg),
+          as.integer(window_end),
+          index,
+          as.integer(index / opts$numinternal * 100),
+          as.integer((index + 1) / opts$numinternal * 100),
+          0L,          
+          cur_record$chr,
+          cur_record$og_start,
+          cur_record$og_end,
+          cur_record$strand
+        )
+      }
+    })
+  return(do.call(rbind, result))
   }
 }
